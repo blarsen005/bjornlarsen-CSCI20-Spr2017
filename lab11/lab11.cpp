767ec449baa3fe390a96d645e17eef54ab1a5569
Bjorn Larsen <blarsen005@student.butte.edu>
2017-01-26 22:50:38 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,8 @@
+/* Step 1: Select a random number N between 1 and 100
+   Step 2: Display "Select a number between 1 and 100"
+   Step 3: Wait for user to input number n between 1 and 100
+           If n is lower than 1 or greater than 100, or if n is not a number, go back to step 2
+   Step 4: If n is equal to N, display "You win!"
+           If n is less than N, display "higher" and go back to step 3
+           If n is greater than N, display "lower" and go back to step 3
+*/
