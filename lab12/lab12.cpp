6d9cacb88991aa4a06c50c89942be0b80170802e
Bjorn Larsen <blarsen005@student.butte.edu>
2017-01-31 22:20:04 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,18 @@
+
+ #include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+     cout<<"            _             "<<endl;
+     cout<<"  _ . - '       ' - . _   "<<endl;
+     cout<<" |'''''''''''''''''''''|  "<<endl;
+     cout<<" |  _____       _____  |  "<<endl;
+     cout<<" | |__|__|     |__|__| |  "<<endl;
+     cout<<" | |__|__|     |__|__| |  "<<endl;
+     cout<<" |                     |  "<<endl;
+     cout<<" |        _____        |  "<<endl;
+     cout<<" |        |   |        |  "<<endl;
+     cout<<" |        |  +|        |  "<<endl;
+     cout<<" |________|___|________|  "<<endl;
+ }
