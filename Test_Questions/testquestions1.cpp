c027419ff14201b0443dbfb1c77ca771e4068c5c
Bjorn Larsen <blarsen005@student.butte.edu>
2017-02-17 00:00:08 +0000

    assignment 1

---
+++ Test_Questions/testquestions1.cpp
@@ -0,0 +1,44 @@
+/*
+1. Q: Which of these is not an objective of this class as defined by the syllabus?
+      A - Design, implement, test, and debug computer programs using basic computation, simple Input/Output (I/O), standard conditional and iterative structures, and functions.
+      B - Practice the C++ programming language up to a high level, be able to quickly and efficiently implement code.
+      C - Summarize the evolution of programming languages and describe how this evolution has led to the programming paradigms in use today.
+      D - Identify and demonstrate different forms of variable binding, visibility, scoping, and lifetime management.
+   A: B
+   
+2. Q: Examine the following instructions:
+       Determine the Base.
+       Determine the height.
+       Multiply base times height.
+      This is an example of a(n) ______ created in order to ______.
+   A: algorithm, find the area of a rectangle.
+   
+3. Q: (T/F) A single-line comment is also known as a block comment.
+   A: False
+   
+4. Q: What two lines of code enable printing of output using the cout construct?
+   A: #include <iostream>
+      using namespace std;
+      
+5. Q: A common error when printing output is to forget to type _____ between items.
+   A: <<
+   
+6. Q: (T/F) The line cin >> var >> endl; will cause an error.
+   A: True
+   
+7. Q: A variable definition  is commonly known as a variable ______.
+   A: declaration
+   
+8. Q: What is the primary purpose of the string variable declaration?
+   A: Storing a sequence of characters.
+   
+9. Q: A struct subitem is called a ______.
+   A: data member
+   
+10. Q: ______ are used to organize a variety of variables into a format that is more understandable.
+       A - Structs
+       B - Objects
+       C - Algorithms
+       D - Strings
+    A: B
+*/
