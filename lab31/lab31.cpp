af7f9360e2040828f3e2c5044170108b417ceae5
Bjorn Larsen <blarsen005@student.butte.edu>
2017-03-23 23:39:52 +0000

    Lab3.1 Submission

--- lab31/lab31.cpp
+++ lab31/lab31.cpp
@@ -2,20 +2,11 @@
 // 3/23/2017
 
 /*
-Problem Statement:
-    Create a program for a new cell phone company that uses complicated formulas to determine the charges for their service.   
-
-    Device charge – free for dumb phone, $5 for smart phone, $10 tablet
-    Monthly charge – $30 for unlimited talk and text
-    Unlimited data – $75 for unlimited talk, text and data (up to 10 GB and $1.50 for each GB after that)
-    Pay per data charge - $5 for the first GB of data and $0.75 for each GB or portion there of up to 5 GB. $1.50 for each GB or portion there of up to 15 GB.  $3 for each GB after that.
-    Corporate Discount – 10% discount for corporate customers
-    Family Plan Discount - $3 per phone line over 2.
-
-    Determine the best plan for the user based on their needs.
-
 Program Description:
-
+    This program determines the best data plan for a user based on a variety of possible conditions. The user inputs number of dumb phones,
+    smart phones, tablets, how much data they plan to use, and if they have corporate discount. The program then determines whether it would be
+    cheapest to use a flat monthly plan with no data, an unlimited data plan, or a pay per data plan. Corporate discount, as well as a family plan
+    discount, are each then applied if conditions are met, and the price of the selected plan is output.
 
 Pseudocode:
     1. Declare variables, including bool variables for the discounts.
@@ -35,18 +26,18 @@ using namespace std;
 int main() {
     
     //Declare variables, including bool variables for the discounts.
-    int numDumbPhones = 0;
+    int numDumbPhones = 0; //Note this is a large mass of variables, and organizing the data into a class would seriouly improve the cleanliness of the program.
     int numSmartPhones = 0;
     int numTablets = 0;
     int deviceCharge = 0;
     double data = 0.00;
-    int monthlyCharge = 0.00;
+    int monthlyCharge = 30.00;
     double unlimitedCharge = 0.00;
     double payPerDataCharge = 0.00;
-    bool corporateDiscount = false;
-    bool familyPlanDiscount = false;
+    char corporateDiscount = 'n';
     double corporateCoeff = 0.00;
     double familyPlanCoeff = 0.00;
+    double totalCharge = 0.00;
     
     //User inputs number/types of devices, data, and discounts.
     cout << "Enter number of dumb phones: " << endl;
@@ -61,49 +52,67 @@ int main() {
     cout << "Enter GB of data: " << endl;
     cin >> data;
     
-    cout << "Have corporate discount? (enter 0 if no)" << endl;
+    cout << "Have corporate discount? (enter 'y' if yes)" << endl;
     cin >> corporateDiscount;
     
     //Equation for device charge based on number on devices.
     deviceCharge = (5 * numSmartPhones) + (10 * numTablets);
     
-    //Monthly charge equation: condition 0 gb data.
-    if (data == 0) {
-        monthlyCharge = 30;
-    }
-    
     //Unlimited data equation INCLUDES condition for going past 10 gb data.
     unlimitedCharge = 75;
-    if (data > 10) {
+    if (data > 10) { //no else statement necessary because unlimited charge has been already given an appropriate value.
         unlimitedCharge = unlimitedCharge + (1.5 * (data - 10));
     }
     
     //Pay per data equation INCLUDES conditions for going past 1, 5, and 15 gb data.
     payPerDataCharge = 5;
-    if (data > 1) {
+    if (data > 1) { //3 if statements here each build off of each other
         payPerDataCharge = payPerDataCharge + (0.75 * (data - 1));
     }
-    
     if (data > 5) {
         payPerDataCharge = payPerDataCharge + (1.5 * (data - 5));
     }
-    
     if (data > 15) {
-        payPerDataCharge = payPerDataCharge + (3.0 * (data - 15))
+        payPerDataCharge = payPerDataCharge + (3.0 * (data - 15));
     }
     
     //Conditions based on if discounts are true.
-    if (corporateDiscount == true) {
-        corporateCoeff = 0.9;
+    if (corporateDiscount == 'y') {
+        corporateCoeff = 0.9; //discounts by taking 90%
     }
     else {
-        corporateCoeff = 1;
+        corporateCoeff = 1; //does not discount by taking 100%
     }
     
-    if (familyPlanCoeff == true) {
-        familyPlanCoeff = 
+    if (numDumbPhones + numSmartPhones + numTablets >= 3) {
+        familyPlanCoeff = 3 * (numDumbPhones + numSmartPhones + numTablets); //discounts by subtracting value
+    }
+    else {
+        familyPlanCoeff = 0; //does not discount by subtracting nothing
     }
     
+    //Condition for each data plan being cheapest, output based on condition.
+    if (data == 0) {
+        totalCharge = (deviceCharge + monthlyCharge) * corporateCoeff - familyPlanCoeff;
+        cout << "Cheapest data plan: Monthly" << endl;
+        cout << "Total charge: " << totalCharge << endl;
+    }
+    
+    else if (unlimitedCharge <= (payPerDataCharge + monthlyCharge)) {
+        totalCharge = (deviceCharge + unlimitedCharge) * corporateCoeff - familyPlanCoeff;
+        cout << "Cheapest data plan: Unlimited" << endl;
+        cout << "Total charge: " << totalCharge << endl;
+    }
+    
+    else if ((payPerDataCharge + monthlyCharge) < unlimitedCharge) {
+        totalCharge = (deviceCharge + payPerDataCharge + monthlyCharge) * corporateCoeff - familyPlanCoeff;
+        cout << "Cheapest data plan: Pay per data" << endl;
+        cout << "Total charge: " << totalCharge << endl;
+    }
+    
+    else { //in case of program error and/or conditions not encompassing all possible inputs
+        cout << "error" << endl;
+    }
     
     return 0;
 }
