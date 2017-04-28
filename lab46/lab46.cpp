ce5272bb7e18d88ffb668e5fd3297c19492171ec
Bjorn Larsen <blarsen005@student.butte.edu>
2017-04-28 01:37:20 +0000

    Lab 4.6 Submission

--- lab46/lab46.cpp
+++ lab46/lab46.cpp
@@ -10,7 +10,7 @@ Problem Statement:
     NOTE: An HTML page is required but it doesn't have to be fancy.  If this is a challenge/problem please come see me ASAP.
     
 Program Description:
-    
+    This program reads a student's grades from a txt file and outputs them into an html file. txt file formatted as seen below.
     
 Pseudocode:
     Main:
@@ -19,38 +19,63 @@ Pseudocode:
         Open files to input
         Read name from file (to name variable)
         Read semester from file (to semester variable)
-        Read units from file (to a units variable)
-        For loop (condition: while class units is one digit)
+        For loop
             Read units from file (to units array)
             Read grade from file (to grades array)
+            grades from letters to numbers
         Read total units from file (to total units variable)
         Read Current GPA from file (to total GPA variable)
-        Grades from letters to numbers
         Semester GPA = (Each grade * each unit) / sum of units
         Total GPA = ((Each grade * each unit) + Prior grade * Prior GPA) / sum of current & prior units
         Output name, semester, semester gpa, and total gpa in HTML format
         Close file
 */
 
-#include <iostream>
+//The format of the input file:
+
+/*
+John Bron Lawn
+
+3
+
+3 A
+
+3 A
+
+3 A
+
+3 B
+
+24 3.5
+*/
+
 #include <string>
-#include <fstream>
+#include <fstream> //1st step of using file i/o: using fstream library
 using namespace std;
+
+const int SIZE = 4; //File contain 4 lines wirth of courses
 
 int main() {
     
-    //string line
-    string line;
-    string name;
-    int semester = 0;
+    string name; //Output to html
+    int semester; //Output to html
+    int units[SIZE];
+    char letterGrades[SIZE];
+    double grades[SIZE];
+    int priorUnits;
+    double priorGPA;
+    double semesterGPA; //Output to html
+    double overallGPA; //Output to html
     
-    //Create fin/fout objects
-    ifstream fin;
-    ofstream fout;
+    //Create fin/fout objects (2nd step of file i/o)
+    ifstream fin; //Both ifstream and ofstream are classes in library <fstream>
+    ofstream fout; //Initializing fin and fout objects allows class functions their class functions to be used
     
-    //Open files to input
+    //Open files to input (3rd step of file i/o)
     fin.open("input.txt");
     fout.open("output.html");
+    
+    //4th step of file i/o: Using the files
     
     //Read name from file (to name variable)
     getline(fin, name);
@@ -58,12 +83,79 @@ int main() {
     //Read semester from file (to semester variable)
     fin >> semester;
     
+    //For loop
+    for (int i = 0; i < SIZE; i++) {
+        
+        //Read units from file (to units array)
+        fin >> units[i];
+        
+        //Read grade from file (to grades array)
+        fin >> letterGrades[i];
+        
+        //Grades from letters to numbers
+        switch (letterGrades[i]) { //A-D associated with 4.0-1.0 GPA
+            
+            case 'A':
+                grades[i] = 4.0;
+                break;
+                
+            case 'B':
+                grades[i] = 3.0;
+                break;
+                
+            case 'C':
+                grades[i] = 2.0;
+                break;
+                
+            case 'D':
+                grades[i] = 1.0;
+                break;
+                
+            default:
+                grades[i] = 0.0;
+                break;
+        }
+    }
     
+    //Read total units from file (to total units variable)
+    fin >> priorUnits;
     
-    cout << name << endl;
-    cout << semester << endl;
+    //Read Current GPA from file (to total GPA variable)
+    fin >> priorGPA;
     
+    //Semester GPA = (Each grade * each unit) / sum of units
+    double topHalf; //top half of semesterGPA fraction
+    double bottomHalf; //bottom half of semesterGPA function
+    for (int i = 0; i < SIZE; i++) {
+        topHalf = topHalf + (units[i] * grades[i]);
+        bottomHalf = bottomHalf + units[i];
+    }
     
+    semesterGPA = topHalf / bottomHalf;
+    
+    //Total GPA = ((Each grade * each unit) + Prior grade * Prior GPA) / sum of current & prior units
+    overallGPA = (topHalf + (priorUnits * priorGPA)) / (bottomHalf + priorUnits);
+    
+    //Output name, semester, semester gpa, and total gpa in HTML format
+    fout << "<!DOCTYPE html>" << endl;
+    fout << "<html>" << endl;
+    fout << "<head>" << endl;
+    fout << "<title>Transcript</title>" << endl;
+    fout << "</head>" << endl;
+    fout << "<body>" << endl;
+    fout << endl;
+    fout << "<h1>Transcript</h1>" << endl;
+    fout << "<p>" << endl;
+    fout << "Name: " << name << endl;
+    fout << "Semester: " << semester << endl;
+    fout << "Semester GPA: " << semesterGPA << endl;
+    fout << "Overall GPA: " << overallGPA << endl;
+    fout << "</p>" << endl;
+    fout << endl;
+    fout << "</body>" << endl;
+    fout << "</html>" << endl;
+    
+    //Close file (5th step of file i/o)
     fin.close();
     fout.close();
     
